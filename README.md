5c05f2ef732a495ec1b6e1ee57516ed8f78694be
wbajwa313 <wbajwa@choice.com.au>
2023-12-06 11:45:40 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# README
+
+This is the Express server API which has two end points.
+
+  * Get Recipes
+  * Save Recipes.
+
+## Deployment
+
+  * Build Command: `yarn`
+  * Start Command: `node app.js`
---
+++ app.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require('fs').promises;
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => res.type('html').send(html));
+
+app.post('/api/saveRecipe', async (req, res) => {
+    const data = req.body;
+    try {
+     await fs.writeFile('./data/recipes.json', JSON.stringify(data));
+      res.status(200).json({ message: 'Data saved successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error saving data' });
+    }
+  });
+  
+  app.get('/api/getRecipes', async (req, res) => {
+    try {
+      const fileData = await fs.readFile('./data/recipes.json', 'utf-8');
+      const jsonData = JSON.parse(fileData);
+      res.status(200).json(jsonData);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error getting data' });
+    }
+  });
+
+const server = app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
+
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Dev Bajwa Server</title>
+    <style>
+      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
+      @font-face {
+        font-family: "neo-sans";
+        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+        font-style: normal;
+        font-weight: 700;
+      }
+      html {
+        font-family: neo-sans;
+        font-weight: 700;
+        font-size: calc(62rem / 16);
+        color: #333;
+      }
+      body {
+        background: white;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: 95dvh;
+        justify-content: space-between;
+      }
+      section {
+        padding: 1em;
+      }
+      li{
+        margin-bottom: 0.25rem;
+      }
+      section span{
+        font-size: 0.6rem;
+        background: #f6f6f6;
+        padding: 0.25rem 0.5rem;
+        border-radius: 0.25em;
+      }
+      footer{
+        text-align: center;
+        padding: 1em;
+        font-size: calc(6rem / 16);
+        background: #f6f6f6;
+        width: 100%;
+      }
+    </style>
+  </head>
+  <body>
+    <section>
+      <h1>Welcome to Dev Bajwa Server</h1>
+      <p>This is the Express Server which serves as an API.</p>
+      <p>It has two end points.</p>
+      <ul>
+        <li>Get Recipes <span><code><a href="/api/getRecipes" target="_blank">/api/getRecipes</a></code></span></li>
+        <li>Save Recipe <span><code>/api/saveRecipe</code></span></li>
+      </ul>
+    </section>
+    <footer>Waqas Naeem Bajwa - Front End Developer</footer>
+  </body>
+</html>
+`
